97ac589bf31a1cd2f11efb047a50efc0645526c7
Greys <j.grzes@greys.co>
2025-10-03 15:26:35 +0900

    feat: Framework v2.0 for WooCommerce 3.0.0 - 4.1.4
    - Extracted from WooCommerce 4.1.4
    - Compatible with WooCommerce 3.0.0 - 4.1.4
    - Includes 22 framework files

--- src/class-wc-api-unit-test-case.php
+++ src/class-wc-api-unit-test-case.php
@@ -30,7 +30,8 @@ class WC_API_Unit_Test_Case extends WC_Unit_Test_Case {
 		$_SERVER['REQUEST_METHOD'] = null;
 
 		// mock the API server to prevent headers from being sent
-		$this->mock_server = $this->getMock( 'WC_API_Server', array ('header' ), array( '/' ) );
+		// $this->mock_server = $this->getMock( 'WC_API_Server', array( 'header' ), array( '/' ) );
+		$this->mock_server = $this->getMockBuilder( 'WC_API_Server' )->setMethods( array( 'header' ) )->disableOriginalConstructor()->getMock();
 
 		WC()->api->register_resources( $this->mock_server );
 	}
@@ -39,10 +40,10 @@ class WC_API_Unit_Test_Case extends WC_Unit_Test_Case {
 	 * Assert the given response is an API error with a specific code and status.
 	 *
 	 * @since 2.2
-	 * @param string $code error code, e.g. `woocommerce_api_user_cannot_read_orders_count`
+	 * @param string   $code error code, e.g. `woocommerce_api_user_cannot_read_orders_count`
 	 * @param int|null $status HTTP status code associated with error, e.g. 400
 	 * @param WP_Error $response
-	 * @param string $message optional message to render when assertion fails
+	 * @param string   $message optional message to render when assertion fails
 	 */
 	public function assertHasAPIError( $code, $status = null, $response, $message = '' ) {
 
@@ -75,5 +76,4 @@ class WC_API_Unit_Test_Case extends WC_Unit_Test_Case {
 		$user->get_role_caps();
 		$user->update_user_level_from_caps();
 	}
-
 }
